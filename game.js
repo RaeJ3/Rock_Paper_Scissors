1b171e5dbb44d9e9b91d7457c6192dc7e9be5ca9
Rae J <128646196+RaeJ3@users.noreply.github.com>
2024-08-30 16:03:19 -0400

    Add if/else statements to switch cases

--- game.js
+++ game.js
@@ -23,34 +23,40 @@ function getHumanChoice(){
 }
 
 
-function playRound(humanSelection, computerSelection) {
-    switch(humanSelection) {
-        case humanSelection === computerSelection: 
+function playRound(humanChoice, computerChoice) {
+    console.log('ran');
+    console.log(humanChoice);
+    console.log(computerChoice);
+    switch(humanChoice) {
+        case computerChoice: 
             console.log("You draw.");
             break;
-        case humanSelection === "rock" && computerSelection === "paper":
-            console.log('You lose! Paper beats Rock!');
-            computerScore++;
+        case "rock":
+            if(computerChoice === "paper") {
+                console.log('You lose! Paper beats Rock!')
+                computerScore++;
+            } else if (computerChoice === "scissors") {
+                console.log('You win! Rock beats Scissors!');
+                humanScore++;
+            };
             break;
-        case humanSelection === "rock" && computerSelection === "scissors":
-            console.log('You win! Rock beats Scissors!');
-            humanScore++;
+        case "paper":
+            if(computerChoice === "scissors") {
+                console.log('You lose! Scissors beats Paper!');
+                computerScore++;
+            } else if (computerChoice === "rock") {
+                console.log('You win! Paper beats Rock!');
+                humanScore++;
+            }
             break;
-        case humanSelection === "paper" && computerSelection === "scissors":
-            console.log('You lose! Scissors beats Paper!');
-            computerScore++;
-            break;
-        case humanSelection === "paper" && computerSelection === "rock":
-            console.log('You win! Paper beats Rock!');
-            humanScore++;
-            break;
-        case humanSelection === "scissors" && computerSelection === "rock":
-            console.log('You lose! Rock beats Scissors!');
-            computerScore++;
-            break;
-        case humanSelection === "scissors" && computerSelection === "paper":
-            console.log('You win! Scissors beats paper!');
-            humanScore++;
+        case "scissors":
+            if(computerChoice === "rock") {
+                console.log('You lose! Rock beats Scissors!');
+                computerScore++;
+            } else if (computerChoice === "paper") {
+                console.log('You win! Scissors beats paper!');
+                humanScore++;
+            }
             break;
     }
     
