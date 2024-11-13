84b75ea4ba2dcbc1959e59373105afb983188590
Rae J <128646196+RaeJ3@users.noreply.github.com>
2024-11-13 14:24:09 -0500

    Declare player/computer points, change player/computer score increment inside playround to player/computer points.

--- game.js
+++ game.js
@@ -1,4 +1,30 @@
-let rounds = 0;
+const rockBtn = document.createElement('button');
+const paperBtn = document.createElement('button');
+const scissorsBtn = document.createElement('button');
+const results = document.createElement('div');
+const scores = document.createElement('div');
+const playerScore = document.createElement('p');
+const computerScore = document.createElement('p');
+const displayBtns = document.createElement('div');
+const body = document.querySelector('body');
+
+rockBtn.textContent = "Rock";
+paperBtn.textContent = "Paper";
+scissorsBtn.textContent = "Scissors";
+playerScore.textContent = "Your score: " + 0;
+computerScore.textContent = "Computer score:" + 0;
+
+body.appendChild(scores);
+body.appendChild(displayBtns);
+body.appendChild(results);
+displayBtns.appendChild(rockBtn);
+displayBtns.appendChild(paperBtn);
+displayBtns.appendChild(scissorsBtn);
+scores.appendChild(playerScore);
+scores.appendChild(computerScore);
+
+
+
 
 function getComputerChoice() {
     let randomChoiceForComputer = Math.random();
@@ -12,84 +38,83 @@ function getComputerChoice() {
 }
 
 
-function getHumanChoice(){
-    let answer = prompt("rock, paper, or scissors?").toLowerCase();
-    if (answer === 'rock' || answer === "paper" || answer === "scissors") {
-        return answer;
-    } else {
-         return answer = prompt("rock, paper, or scissors?").toLowerCase();
-    }
-}
+
+// const humanSelection = function getHumanChoice(){
+//     let answer = prompt("rock, paper, or scissors?").toLowerCase();
+//     if (answer === 'rock' || answer === "paper" || answer === "scissors") {
+//         return answer;
+//     } else {
+//          return answer = prompt("rock, paper, or scissors?").toLowerCase();
+//     }
+// }
 
 
 function playGame() {
     let humanScore = 0;
     let computerScore = 0;
-    console.log("Your Score: " + humanScore);
-    console.log("Computer Score: " + computerScore);
-    
+
     function playRound(humanChoice, computerChoice) {
-    rounds++;
     
-    console.log("Computer choice: " + computerChoice);
+    //results.textContent = "Computer choice: " + computerChoice;
 
     switch(humanChoice) {
         case computerChoice: 
-            console.log("You draw.");
+            results.textContent = "You draw.";
             break;
         case "rock":
             if(computerChoice === "paper") {
-                console.log('You lose! Paper beats Rock!')
+                results.textContent ='You lose! Paper beats Rock!';
                 computerScore++;
+                computerScore.textContent += computerScore;
             } else if (computerChoice === "scissors") {
-                console.log('You win! Rock beats Scissors!');
+                results.textContent = 'You win! Rock beats Scissors!';
                 humanScore++;
             };
             break;
         case "paper":
             if(computerChoice === "scissors") {
-                console.log('You lose! Scissors beats Paper!');
+                results.textContent = 'You lose! Scissors beats Paper!';
                 computerScore++;
             } else if (computerChoice === "rock") {
-                console.log('You win! Paper beats Rock!');
+                results.textContent = 'You win! Paper beats Rock!';
                 humanScore++;
             }
             break;
         case "scissors":
             if(computerChoice === "rock") {
-                console.log('You lose! Rock beats Scissors!');
+                results.textContent = 'You lose! Rock beats Scissors!';
                 computerScore++;
             } else if (computerChoice === "paper") {
-                console.log('You win! Scissors beats paper!');
+                results.textContent = 'You win! Scissors beats paper!';
                 humanScore++;
             }
             break;
+
+            playerScore.textContent = "Your Score: " + humanScore;
+            computerScore.textContent = "Computer Score: " + computerScore;
+                if(humanScore == computerScore) {
+                    results.textContent = "You Draw! Final Score: " + humanScore + " : " + computerScore;
+                } else if (humanScore > computerScore) {
+                    results.textContent = "You Win! Final Score: " + humanScore + " : " + computerScore;
+                } else if (humanScore < computerScore) {
+                    results.textContent = "You Lose! Final Score: " + humanScore + " : " + computerScore;
+                }
     }
     
 
 }
 
-    for(let i=0;i<5;i++) {
-        console.log(rounds);
-        playRound(getHumanChoice(), getComputerChoice());
-            console.log("Your Score: " + humanScore);
-            console.log("Computer Score: " + computerScore);
-            switch (rounds) {
-                case 5:
-                    console.log(rounds);
+        rockBtn.addEventListener('click', () => {
+            playRound('rock', getComputerChoice());
+        })
+        paperBtn.addEventListener('click', () => {
+            playRound('paper', getComputerChoice())
+        })
+        scissorsBtn.addEventListener('click', () => {
+            playRound('scissors', getComputerChoice())
+        })
 
-                    if(humanScore == computerScore) {
-                        console.log("You Draw! Final Score: " + humanScore + " : " + computerScore);
-                    } else if (humanScore > computerScore) {
-                        console.log("You Win! Final Score: " + humanScore + " : " + computerScore);
-                    } else if (humanScore < computerScore) {
-                        console.log("You Lose! Final Score: " + humanScore + " : " + computerScore)
-                    }
-                    break;
-                    default:
-            }
-            
-        }
-    }
-
+        playerScore.textContent += playerScore;
+        computerScore.textContent += computerScore;
+}
 console.log(playGame());
